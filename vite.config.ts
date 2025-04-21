87aa005bfefae1763ed2648036aba2181db694bd
Gui-Heliodoro <109614821+Gui-Heliodoro@users.noreply.github.com>
2025-04-21 17:26:27 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/farmacia_ia_v10/',      // ← aqui
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
